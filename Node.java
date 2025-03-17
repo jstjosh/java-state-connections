59c362bf1be24bf296200a55a36e6a27e95c49d4
jstjosh <joshwajoe345@gmail.com>
2025-03-16 22:49:15 -0500

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,15 @@
+java
+import java.util.List;
+
+public class Node {
+    String stateName;
+    List<String> neighbors;
+    Node next;
+
+    // Constructor to initialize the node
+    public Node(String stateName, List<String> neighbors) {
+        this.stateName = stateName;
+        this.neighbors = neighbors;
+        this.next = null;
+    }
+}
